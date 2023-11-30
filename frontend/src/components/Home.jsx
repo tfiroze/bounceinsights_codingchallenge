65fc0ba1e7586abd8edb63b221261b0b5dc53c93
Tehsein Firoze <73216889+tfiroze@users.noreply.github.com>
2023-11-30 12:58:29 +0000

    UI changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -16,11 +16,11 @@ const Home = () => {
     setLoading(true);
     try {
       const response = await axios.get(`https://backend-bounceinsights-codingchallenge.onrender.com/api/countries/${country}`);
-      console.log(response.data)
       setCountryInfo(response.data[0]);
     } catch (error) {
       setError('Failed to fetch country data');
       setCountryInfo(null);
+      console.error(error); 
     } finally {
       setLoading(false);
     }
@@ -28,10 +28,14 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 p-4 text-center">
-      <h1 className="text-5xl font-bold mb-6">
-        Hello, welcome to the Bounce Insights Coding Challenge!
+    <div className="mb-8 p-4 w-full max-w-2xl bg-indigo-600 rounded-lg shadow-xl">
+      <h1 className="text-4xl font-bold text-white">
+        Bounce Insights Coding Challenge
       </h1>
-      <p className="text-xl mb-4">Please Enter Your Country</p>
+    </div>
+    <div className="text-2xl font-medium mb-4 text-gray-100 bg-indigo-600 p-2 rounded-md shadow">
+      Please Enter Your Country
+    </div>
       <div className="flex flex-wrap justify-center items-center gap-4">
         <input
           type="text"
@@ -48,28 +52,48 @@ const Home = () => {
           {loading ? 'Loading...' : 'Go'}
         </button>
       </div>
-      {error && <p className="text-white mt-2">{error}</p>}
+      {error && <p className="text-white text-2xl mt-2">{error}</p>}
+
       {countryInfo && (
-        <div className="mt-8 bg-white text-gray-800 rounded-xl shadow-lg max-w-2xl w-full overflow-hidden">
-    <div className="p-5">
-      <h2 className="text-3xl font-bold mb-2 text-gray-800">{countryInfo?.name?.common}</h2>
-      <div className="text-gray-700">
-        <p className="mb-1"><strong>Official Name:</strong> {countryInfo?.name?.official}</p>
-        <p className="mb-1"><strong>Capital:</strong> {countryInfo?.capital}</p>
-        <p className="mb-1"><strong>Region:</strong> {countryInfo?.region}</p>
-        <p className="mb-1"><strong>Subregion:</strong> {countryInfo?.subregion}</p>
-        <p className="mb-1"><strong>Languages:</strong> {Object.values(countryInfo?.languages).join(', ')}</p>
-        <p className="mb-1"><strong>Currencies:</strong> {Object.entries(countryInfo?.currencies).map(([code, { name, symbol }]) => `${name} (${symbol})`).join(', ')}</p>
-        <p className="mb-1"><strong>Population:</strong> {countryInfo?.population?.toLocaleString()}</p>
-        <p className="mb-1"><strong>Timezones:</strong> {countryInfo?.timezones?.join(', ')}</p>
-        <p className="mb-1"><strong>Continents:</strong> {countryInfo?.continents?.join(', ')}</p>
+      <div className="mt-8 mb-8 p-5 bg-white text-gray-800 rounded-xl shadow-lg max-w-2xl w-full mx-auto">
+        <div className="flex flex-col items-center mb-4">
+          <h2 className="text-3xl font-bold text-center">{countryInfo?.name?.common}</h2>
+          <img src={countryInfo?.flags?.png} alt={`Flag of ${countryInfo?.name?.common}`} className="my-4 h-20"/>
+        </div>
+        <div className="border border-gray-300 rounded-lg">
+          <div className="grid grid-cols-2 divide-y divide-gray-300">
+            <p className="border-r p-2 font-semibold">Official Name:</p>
+            <p className="p-2">{countryInfo?.name?.official}</p>
+
+            <p className="border-r p-2 font-semibold">Capital:</p>
+            <p className="p-2">{countryInfo?.capital}</p>
+
+            <p className="border-r p-2 font-semibold">Region:</p>
+            <p className="p-2">{countryInfo?.region}</p>
+
+            <p className="border-r p-2 font-semibold">Subregion:</p>
+            <p className="p-2">{countryInfo?.subregion}</p>
+
+            <p className="border-r p-2 font-semibold">Languages:</p>
+            <p className="p-2">{Object.values(countryInfo?.languages).join(', ')}</p>
+
+            <p className="border-r p-2 font-semibold">Currencies:</p>
+            <p className="p-2">
+              {Object.entries(countryInfo?.currencies).map(([code, { name, symbol }]) => `${name} (${symbol})`).join(', ')}
+            </p>
+
+            <p className="border-r p-2 font-semibold">Population:</p>
+            <p className="p-2">{countryInfo?.population?.toLocaleString()}</p>
+
+            <p className="border-r p-2 font-semibold">Timezones:</p>
+            <p className="p-2">{countryInfo?.timezones?.join(', ')}</p>
+
+            <p className="border-r p-2 font-semibold">Continents:</p>
+            <p className="p-2">{countryInfo?.continents?.join(', ')}</p>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="p-5 border-t border-gray-200">
-      <img src={countryInfo?.flags?.png} alt={`Flag of ${countryInfo?.name?.common}`} className="mx-auto h-20"/>
-    </div>
-  </div>
-    )}
+      )}
     </div>
   );
 }
